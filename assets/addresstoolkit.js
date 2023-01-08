6b7fd4f8f9bee06e39568d903b461935ed121ef9
Devin Price <devin@wptheming.com>
2023-01-08 10:44:49 -0600

    Start parsing of address.

--- assets/addresstoolkit.js
+++ assets/addresstoolkit.js
@@ -9,7 +9,6 @@ class AddressToolkit {
 	}
 
 	initFields = () => {
-		console.log("init");
 		const billingField = document.getElementById("billing_address_1");
 		if (billingField) {
 			this.initAutocomplete(billingField, "billing");
@@ -17,16 +16,57 @@ class AddressToolkit {
 
 		const shippingField = document.getElementById("shipping_address_1");
 		if (shippingField) {
-			this.initAutocomplete(billingField, "shipping");
+			this.initAutocomplete(shippingField, "shipping");
 		}
 	};
 
 	initAutocomplete = (field, type) => {
-		console.log(field, type);
 		const country = document.getElementById(`${type}_country`);
 		const state = document.getElementById(`${type}_state`);
 		const city = document.getElementById(`${type}_city`);
 		const postcode = document.getElementById(`${type}_postcode`);
+
+		// Initialize Places Autocomplete on Address 1.
+		const address = new google.maps.places.Autocomplete(field, {
+			types: ["address"],
+			fields: ["address_component"],
+		});
+
+		// If the country value field is set, we'll only return results from that country.
+		this.setCountryRestriction(address, country.value);
+		country.addEventListener("change", () => {
+			this.setCountryRestriction(address, country.value);
+		});
+
+		// Listen for an autocomplete selection.
+		google.maps.event.addListener(address, "place_changed", () => {
+			this.parsePlace(address);
+		});
+	};
+
+	setCountryRestriction = (address, country) => {
+		if (!country) {
+			return;
+		}
+
+		// @TODO This is where we could include or exclude countries based on user settings.
+
+		address.setComponentRestrictions({
+			country: country,
+		});
+	};
+
+	parsePlace = (address) => {
+		console.log("place change");
+		let place = address.getPlace();
+		let address1 = "";
+		let address2 = "";
+		console.log(place);
+
+		for (let i = 0; i < place.address_components.length; i++) {
+			const type = place.address_components[i].types[0];
+			console.log(type);
+		}
 	};
 }
 
